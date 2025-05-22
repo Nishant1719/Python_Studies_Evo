a70ece7cf82e3c3f135a59e9c16094ad6671275f
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-22 15:47:52 +0530

    Fixes

--- 02_conditional/solution_06.py
+++ 02_conditional/solution_06.py
@@ -2,7 +2,7 @@ distance = float(input("Enter the distance in km: "))
 
 if distance < 3:
     transportation = "Walk"
-elif distance < 15:
+elif distance <= 15:
     transportation = "Bike"
 else:
     transportation = "Car"
