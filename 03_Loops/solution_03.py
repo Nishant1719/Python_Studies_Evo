1c541b31c2fa82d8d23425f3036d04653a98967c
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-25 21:16:14 +0530

    solved

--- 03_Loops/solution_03.py
+++ 03_Loops/solution_03.py
@@ -1,1 +1,8 @@
 # Problem: Print the multiplication table for a given number up to 10, but skip the fifth iteration.
+number = int(input("Enter the number: "))
+for n in range(1,11):
+    if n == 5:
+        continue
+    else:
+        print(number*n)
+
