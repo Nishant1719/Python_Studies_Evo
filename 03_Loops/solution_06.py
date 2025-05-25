ccffc517b23aae17d49cd4460df97f3a04921255
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-26 01:44:04 +0530

    solved

--- 03_Loops/solution_06.py
+++ 03_Loops/solution_06.py
@@ -1,1 +1,20 @@
-# Problem: Compute the factorial of a number using a while loop.
+# Problem: Compute the factorial of a number using a (while loop).
+# my sol: from for loop
+num = int(input("Enter the number: "))
+
+# result = num 
+# for n in range(num):
+#     if (num - 1) != 0:
+#         result = result * (num - 1)
+#         num-=1
+# print(result)
+
+# While loop
+
+result = num
+while num>1:
+     if (num - 1) != 0:
+        result = result * (num - 1)
+        num -= 1
+
+print(result)
