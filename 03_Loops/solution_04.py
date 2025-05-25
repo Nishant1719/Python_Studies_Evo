faae117962d4bc02180cc3285e1f0c564fd64e31
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-25 21:27:14 +0530

    solved

---
+++ 03_Loops/solution_04.py
@@ -0,0 +1,8 @@
+# Problem: Reverse a string using a loop.
+input_string = input("Enter the string: ")
+result_string = ""
+
+for i in range(1,len(input_string)):
+    result_string = result_string + input_string[-i]
+    
+print(result_string + input_string[0]) 
