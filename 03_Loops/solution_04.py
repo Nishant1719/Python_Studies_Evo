8485f35672907ff3a5cf57de7d6bb9abbf4a5043
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-25 22:21:59 +0530

    Updated sol

--- 03_Loops/solution_04.py
+++ 03_Loops/solution_04.py
@@ -2,7 +2,13 @@
 input_string = input("Enter the string: ")
 result_string = ""
 
-for i in range(1,len(input_string)):
-    result_string = result_string + input_string[-i]
+# for i in range(1,len(input_string)):
+#     result_string = result_string + input_string[-i]
     
-print(result_string + input_string[0]) 
+# print(result_string + input_string[0]) 
+
+# Simple sol:
+for char in input_string:
+    result_string = char + result_string
+
+print(result_string)
