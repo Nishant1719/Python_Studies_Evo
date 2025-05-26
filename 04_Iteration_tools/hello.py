68ba06a33162a57e317448f786ed9a0c4d8eb606
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-26 13:31:25 +0530

    Updates

---
+++ 04_Iteration_tools/hello.py
@@ -0,0 +1,25 @@
+import time
+print("Hello world")
+username = "Nishant"
+
+time.sleep(3)
+print(username)
+
+# Terminal : Opening file in terminal
+# >>> for line in open('hello.py'):
+# ...     print(line,end='')
+# ... 
+# import time
+# print("Hello world")
+# username = "Nishant"
+
+# time.sleep(3)
+# print(username)
+
+# Using while loop:
+# f = open('hello.py')
+
+# while True:
+#     line = f.readline()
+#     if not line: break
+#     print(line)
