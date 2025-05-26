1b40e85930168ee4ba6fc2a3d8a25056dffd0d17
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-26 12:45:52 +0530

    solved

--- 03_Loops/solution_10.py
+++ 03_Loops/solution_10.py
@@ -1,2 +1,13 @@
 # Problem: Implement an exponential backoff strategy that doubles the wait time between retries, starting from 1 second, but stops after 5 retries.
+import time
 
+max_tries = 5
+attempts = 1
+wait_time = 1
+
+while attempts <= max_tries:
+    print(f"Attempts {attempts} - wait time {wait_time}")
+    time.sleep(wait_time)
+    wait_time *= 2
+    attempts+=1
+    
