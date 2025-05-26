ba0c54234063e73643b79b99093810614b5055b5
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-26 12:36:25 +0530

    solved

--- 03_Loops/solution_09.py
+++ 03_Loops/solution_09.py
@@ -1,11 +1,22 @@
 # Problem: Check if all elements in a list are unique. If a duplicate is found, exit the loop and print the duplicate.
 items = ["apple", "banana", "orange", "apple", "mango"]
 
-for i in items:
-    count = items.count(i)
-    if count > 1:
-        print(i)
-        break
-    else:
-        print("All unique")
+# for i in items:
+#     count = items.count(i)
+#     if count > 1:
+#         print(i)
+#         break
+#     else:
+#         print("All unique")
         
+# ---------------------------
+
+unique_items = set()
+
+for i in items:
+    if i in unique_items:
+        print("Dup :",i)
+    else:
+        unique_items.add(i)
+    
+print(unique_items)
