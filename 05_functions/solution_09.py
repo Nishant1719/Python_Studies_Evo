ac2fb8165dd2b149b2208cdb77f8de1a41cfa8b8
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 15:52:39 +0530

    yield

---
+++ 05_functions/solution_09.py
@@ -0,0 +1,19 @@
+# Problem: Write a generator function that yields even numbers up to a specified limit.
+
+def even_generator(limit):
+    for i in range(2, limit + 1, 2):
+        yield i
+        
+# yield : # The yield statement is used to produce a value from the generator function.
+    # - It stores the state of the function, allowing it to resume where it left off.
+    # - This makes generators more memory efficient than regular functions, especially for large datasets.
+    # - different from return, which exits the function and does not allow resuming.
+
+# usage example
+# print(even_generator(10))  # <generator object even_generator at 0x...>
+# To get the values from the generator, you can use a loop or convert it to a list.
+# print(list(even_generator(10)))  # [2, 4, 6, 8, 10]
+for num in even_generator(10):
+    print(num)
+    
+    
