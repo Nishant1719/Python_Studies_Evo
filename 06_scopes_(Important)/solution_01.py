da476e2239208b8a7367831949d6bc3ea451f171
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-28 00:36:45 +0530

    Update examples in solution_01.py to demonstrate closure function behavior with multiple instances

--- 06_scopes_(Important)/solution_01.py
+++ 06_scopes_(Important)/solution_01.py
@@ -40,5 +40,11 @@ def main_func(num):
 # as sub_func is returned from main_func, it retains access to num. plus its not called yet.
 
 result = main_func(2)
-print(result(3)) # Output : It will be sqrt of 3 as per the func defination.
+result_2 = main_func(3)
+print(result(3)) # Output : 9, as per the func definition (3 ** 2).
+print(result_2(3)) # Output : 27, as per the func definition (3 ** 3).
 
+# Incase of result : result holds a reference to the sub_func function, which has access to the num variable which is 2. in this case.
+# Incase of result_2 : result_2 holds a reference to the sub_func function, which has access to the num variable which is 3. in this case.
+# So, when we call result(3), it returns 3 raised to the power of 2, and when we call result_2(3), it returns 3 raised to the power of 3.
+# This is a powerful feature of Python, allowing you to create functions that can remember values from their enclosing scope.
