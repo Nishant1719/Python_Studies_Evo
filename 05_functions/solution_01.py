87104ebf9c8a297061faa3bc75e042583ea5332e
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 14:25:25 +0530

    different way of solution

--- 05_functions/solution_01.py
+++ 05_functions/solution_01.py
@@ -1,7 +1,10 @@
 # Problem: Write a function to calculate and return the square of a number.
 
+# def cal_sqr(num):
+#     return print(num * num)
+
 def cal_sqr(num):
-    return print(num * num)
+    return print(num ** 2)
 
 n = int(input("Enter the number: "))
 cal_sqr(n)
