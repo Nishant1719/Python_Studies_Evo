0a02deddc4aec77437b635a759fc97eebab00860
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 20:52:50 +0530

    Implement recursive factorial function and add print statement for testing

--- 05_functions/solution_10.py
+++ 05_functions/solution_10.py
@@ -1,1 +1,10 @@
 # Problem: Create a recursive function to calculate the factorial of a number.
+
+def my_factorial(number):
+    if number==0: return 1
+    else: return number * my_factorial(number-1)
+
+print(my_factorial(5))
+
+# Always find how to exit the function 
+# Exit strategies are important in the recursive function
