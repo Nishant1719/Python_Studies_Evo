342bde1f91369bba46fb1dbe6179e15d6dadb0b2
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 15:04:05 +0530

    Created First lambda in py

--- 05_functions/solution_06.py
+++ 05_functions/solution_06.py
@@ -1,1 +1,10 @@
 # Problem: Create a lambda function to compute the cube of a number.
+# Lambda Functions are basically nameless functions 
+# Uses : if the function is not gonna be used soo many times :
+# Url  : https://www.youtube.com/watch?v=JptOj-D6gtw&list=PLu71SKxNbfoBsMugTFALhdLlZ5VOqCg2s&index=16
+# timestamp : 23:00
+
+
+cube = lambda x: x ** 3 
+# print(cube) # Stores memory address.
+print(cube(3)) # Prints values.
