fb3b36e51843d17d0c8a808b0e766f955df94c3b
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 15:38:29 +0530

    Add function to print keyword arguments in key: value format

--- 05_functions/solution_08.py
+++ 05_functions/solution_08.py
@@ -1,1 +1,9 @@
 # Problem: Create a function that accepts any number of keyword arguments and prints them in the format key: value.
+
+def print_games(**kwargs):
+    for key,value in kwargs.items():
+        print(f"Keys: {key} Value: {value}") # Formatting string
+    
+# Usage:
+print_games(games1 = "God of war",games2 = "God of war 2",games3 = "God of war 3")
+
