bea8a68a99129f4d969d3d96f745a1dbe02b21ba
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-27 15:55:19 +0530

    Difference btw Return and yield

--- 05_functions/solution_09.py
+++ 05_functions/solution_09.py
@@ -1,5 +1,10 @@
 # Problem: Write a generator function that yields even numbers up to a specified limit.
 
+# def even_generator(limit):
+#     for i in range(2, limit + 1, 2):
+#         return i
+
+
 def even_generator(limit):
     for i in range(2, limit + 1, 2):
         yield i
@@ -10,9 +15,12 @@ def even_generator(limit):
     # - different from return, which exits the function and does not allow resuming.
 
 # usage example
+# print(even_generator(10))  # returned 2 , When used return keyword.
 # print(even_generator(10))  # <generator object even_generator at 0x...>
 # To get the values from the generator, you can use a loop or convert it to a list.
 # print(list(even_generator(10)))  # [2, 4, 6, 8, 10]
+
+
 for num in even_generator(10):
     print(num)
     
