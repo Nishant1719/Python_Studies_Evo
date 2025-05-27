749212c0f2b7cd9dcb1e4ce5eba8c27776ff140b
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-28 01:52:21 +0530

    Add debug decorator to print function name and arguments

--- Decorators/solution_01.py
+++ Decorators/solution_01.py
@@ -1,10 +1,29 @@
 import time 
 
 def timer(func):
-    def wrapper(*args, **kwargs): # *args and **kwargs are used to pass any number of arguments.
-                                  # *args is a tuple of positional arguments and **kwargs is a dictionary of keyword arguments.
-        result = func(*args, **kwargs)
+    def wrapper(*args, **kwargs): # why parameters are passed in wrapper?
+        # Because we want to pass any number of arguments to the original function.
+        # but didnt func already have parameters?
+        # Yes, but when we use the @timer decorator, we want to be able to pass any arguments to the original function.
+        # meaning, we want to be able to call the original function with any number of arguments.
+        # So, we use *args and **kwargs to pass any number of positional and keyword arguments to the original function.
+        start_time = time.time()              
+        result = func(*args, **kwargs) # we are not even passing any dict ir list here, so why **kwargs its not giving error?
+        # Because **kwargs is used to pass keyword arguments to the original function, and it can be empty. 
+        # So, even if we don't pass any keyword arguments, it will still work.
+        # If we had used only **kwargs, it would have given an error if we tried to pass positional arguments.
+        end_time = time.time()
+        print(f"{func.__name__} Start time : {start_time} ; End time : {end_time} Result : {result}")
         return result
     return wrapper
 
-    
+@timer
+def cal(a,b):
+    return a+b
+
+# print(cal(2,3))
+# Example usage:
+timer(cal)(10, 20)  # This will call the cal function with arguments 10 and 20 and print the time taken.
+
+# time_taken = timer(cal)
+# time_taken(10, 20)
---
+++ Decorators/solution_02.py
@@ -0,0 +1,48 @@
+# Problem: Create a decorator to print the function name and the values of its arguments every time the function is called.
+
+# solution :
+def debug(func):
+    def wrapper(*args, **kwargs):
+        print(f"Function Name : {func.__name__} & its arguments {args,kwargs}")
+        return func(*args, **kwargs)
+    return wrapper 
+
+@debug
+def greet(name,message,greeting="Hello"):
+    return print(f"{name}, {greeting} Message : {message}")
+    
+greet("Nishant",greeting="Namaste",message="Hi there people")
+
+# ------------------------------------------------------------------------------------
+# Examples with no arguements:
+# def debug_no_parameters(func):
+#     def wrapper():
+#         print("Decorator is called!")
+#         return func()
+#     return wrapper 
+
+# @debug_no_parameters
+# def print_my_name():
+#     return print("Nishant")
+
+# print_my_name()
+
+# ------------------------------------------------------------------------------------
+# Example:
+# def show(func):
+#     def wrapper(*args, **kwargs):
+#         result = func(*args,**kwargs)
+#         print(f"function Name : {func.__name__} Arguments {args} result : {result}")
+#         return result
+#     return wrapper
+
+# @show
+# def cal(a,b):
+#     return a+b
+
+# Example usage:
+# show(cal)(10, 20)  # This will call the cal function with arguments 10 and 20 and print the function name, arguments, and result.
+# result = cal(2, 3)  # This will print the function name, arguments, and result.
+# print(result)
+
+
