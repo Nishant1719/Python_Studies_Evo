93740f82b1397afb6bd3433da272f2473f817fef
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-28 13:32:54 +0530

    Refactor main loop into a dedicated main function for better structure and readability

--- 07_Error_Handling/youtube_manager.py
+++ 07_Error_Handling/youtube_manager.py
@@ -12,26 +12,30 @@ def delete_video():
 
 videos = []
 
-while True:
-    print("Choose the following options:")
-    print("1. List of all the fav. videos")
-    print("2. Add new video to the list")
-    print("3. Update the video list")
-    print("4. Delete the video")
-    print("5. Exit")
-    
-    choice =  input("Please Enter Your Choice")
-    match choice:
-        case '1':
-            list_all_videos(videos)
-        case '2':
-            add_video(videos)
-        case '3':
-            update_video(videos)
-        case '4':
-            delete_video(videos)
-        case '5':
-            break
-        case _:
-            print("Invalid Choice!")
-            break
+def main():
+    while True:
+        print("Choose the following options:")
+        print("1. List of all the fav. videos")
+        print("2. Add new video to the list")
+        print("3. Update the video list")
+        print("4. Delete the video")
+        print("5. Exit")
+        
+        choice =  input("Please Enter Your Choice")
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice!")
+
+# This is a common Python idiom that allows the script to be run as a standalone program or imported as a module without executing the main function.
+if __name__ == "__main__":
+    main() 
