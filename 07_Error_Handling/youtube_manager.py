0597fd41344231fe2045426c9448aa1dde1dc890
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-05-28 13:27:17 +0530

    Basic Structure of the application

---
+++ 07_Error_Handling/youtube_manager.py
@@ -0,0 +1,37 @@
+def list_all_videos():
+    pass
+
+def add_video():
+    pass
+
+def update_video():
+    pass
+
+def delete_video():
+    pass
+
+videos = []
+
+while True:
+    print("Choose the following options:")
+    print("1. List of all the fav. videos")
+    print("2. Add new video to the list")
+    print("3. Update the video list")
+    print("4. Delete the video")
+    print("5. Exit")
+    
+    choice =  input("Please Enter Your Choice")
+    match choice:
+        case '1':
+            list_all_videos(videos)
+        case '2':
+            add_video(videos)
+        case '3':
+            update_video(videos)
+        case '4':
+            delete_video(videos)
+        case '5':
+            break
+        case _:
+            print("Invalid Choice!")
+            break
